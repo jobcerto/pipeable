60e8d12781bcee48c83648d04069ae73ffc9bcfa
Eduardo Geschonke <edu@jobcerto.com>
2018-05-20 01:12:12 -0300

    First Draft

---
+++ src/PipeableServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Jobcerto\Pipeable;
+
+use Illuminate\Support\ServiceProvider;
+
+class PipeableServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot() {}
+}
